666401ae957129c43539d5c2ba4124247816bd8f
violin-ing <eugenekhy@gmail.com>
2025-09-16 08:54:00 +0100

    edited files

---
+++ Tutorials/tutorial-01/recursive-fib.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+int fib(int n) {
+    if (n <= 1) {
+        return n;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cout << "Usage: ./recursive-fib <integer>" << std::endl;
+        return 1;
+    }
+
+    int n = std::stoi(argv[1]);
+    std::cout << fib(n) << std::endl;
+
+    return 0;
+}
