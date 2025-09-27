d7b99b4b24a9533d36dffcca713846eb4e0b1dd5
Eugene Kong <ek242@st-andrews.ac.uk>
2025-09-27 13:35:30 +0100

    completed prac-1

--- Practicals/practical-01-rr-scheduler/rr.cpp
+++ Practicals/practical-01-rr-scheduler/rr.cpp
@@ -6,7 +6,6 @@
  * Tom Spink <tcs6@st-andrews.ac.uk>
  */
 #include <stacsos/kernel/sched/alg/rr.h>
-#include <algorithm>
 
 // *** COURSEWORK NOTE *** //
 // This will be where you are implementing your round-robin scheduling algorithm.
@@ -16,20 +15,69 @@
 using namespace stacsos::kernel::sched;
 using namespace stacsos::kernel::sched::alg;
 
-
 // void round_robin::add_to_runqueue(tcb &tcb) { panic("TODO"); }
-void round_robin::add_to_runqueue(tcb &tcb) {
-
+void round_robin::add_to_runqueue(tcb &task) {
+	rr_node *node = new rr_node(&task);
+	if (!tail) { // Empty list
+		head = tail = node;
+	} else {
+		tail->next = node;
+		tail = node;
+	}
 }
 
 
 // void round_robin::remove_from_runqueue(tcb &tcb) { panic("TODO"); }
-void round_robin::remove_from_runqueue(tcb &tcb) {
+void round_robin::remove_from_runqueue(tcb &task) {
+	rr_node *prev = nullptr;
+	rr_node *curr = head;
 
+	// Iterate through the linked list to find the task
+	while (curr) {
+		if (curr->task == &task) {
+			if (prev) {
+				prev->next = curr->next;
+			} else { // If the task to be removed is the first in the queue
+				head = curr->next;
+			}
+
+			if (curr == tail) { // If the task to be removed is the last in the queue
+				tail = prev;
+			}
+
+			delete curr;
+			return;
+		}
+		prev = curr;
+		curr = curr->next;
+	}
 }
 
 
 // tcb *round_robin::select_next_task(tcb *current) { panic("TODO"); }
 tcb *round_robin::select_next_task(tcb *current) {
+	if (!head) { // Empty queue
+		return nullptr;
+	}
 
+	// Pop the first task in the queue
+	rr_node *node = head;
+	head = head->next;
+	if (!head) {
+		tail = nullptr;
+	}
+
+	tcb *task = node->task;
+
+	// Push to the end of the queue
+	node->next = nullptr;
+	if (!tail) {
+		head = tail = node;
+	} else {
+		tail->next = node;
+		tail = node;
+	}
+
+	return task;
 }
+
--- Practicals/practical-01-rr-scheduler/rr.h
+++ Practicals/practical-01-rr-scheduler/rr.h
@@ -8,23 +8,33 @@
 #pragma once
 
 #include <stacsos/kernel/sched/alg/scheduling-algorithm.h>
-#include <list>
-#include <unordered_map>
-
 
 namespace stacsos::kernel::sched::alg {
 
 // *** COURSEWORK NOTE *** //
 // Please edit this file in any way you see fit.
 
+// Use a linked list to implement our queue struture
+struct rr_node {
+	tcb *task;
+	rr_node *next;
+	rr_node(tcb *t) : task(t), next(nullptr) {}
+};
+
 class round_robin : public scheduling_algorithm {
 private:
-    std::list<tcb*> runqueue;
-    std::unordered_map<tcb*, size_t> schedule_count;
+	rr_node *head; // First task in run queue
+	rr_node *tail; // Last task in run queue
+
 public:
-    virtual void add_to_runqueue(tcb &tcb) override;
-    virtual void remove_from_runqueue(tcb &tcb) override;
-    virtual tcb *select_next_task(tcb *current) override;
-    virtual const char *name() const { return "round robin"; }
+	// Constructor
+	round_robin() : head(nullptr), tail(nullptr) {}
+
+	virtual void add_to_runqueue(tcb &tcb) override;
+	virtual void remove_from_runqueue(tcb &tcb) override;
+	virtual tcb *select_next_task(tcb *current) override;
+	virtual const char *name() const { return "round robin"; }
 };
 } // namespace stacsos::kernel::sched::alg
+
+
