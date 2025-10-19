8dda1f42b1f6d3daa316b319c6a6ae1da2ae56d8
Eugene Kong <ek242@st-andrews.ac.uk>
2025-10-19 23:53:14 +0100

    SOLVED PRAC 2

--- Practicals/practical-02-allocator/mem/address-space-region.cpp
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: MIT */
-
-/* StACSOS - Kernel
- *
- * Copyright (c) University of St Andrews 2024
- * Tom Spink <tcs6@st-andrews.ac.uk>
- */
-#include <stacsos/kernel/mem/address-space-region.h>
